0f6bda3275a261bb209dedc25e293def2e696fbc
Nguyen Danh <nguyencongdanh872001@gmail.com>
2022-04-06 21:05:40 +0700

    fix async await index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,7 @@ import Layout from "../components/layout/LayoutHome";
 import ListProduct from "../components/listproduct/ListProduct";
 import SliderBanner from "../components/slider/SliderBanner";
 
-const Home: NextPage = ({
-  freebase,
-  saltnic,
-  podmod,
-  boxtank,
-  phukien,
-  disposablepod,
-  banner,
-}: any) => {
+const Home: NextPage = ({ jsons }: any) => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -30,30 +22,30 @@ const Home: NextPage = ({
         />
       </Head>
       <div className="pb-10 pt-[58px] lg:pt-[8px] ">
-        <SliderBanner banner={banner} />
+        <SliderBanner banner={jsons[0]} />
         <Category />
         <ListProduct
           heading="freebase"
-          data={freebase.product}
+          data={jsons[1].product}
           link="freebase"
         />
-        <ListProduct heading="saltnic" data={saltnic.product} link="saltnic" />
+        <ListProduct heading="saltnic" data={jsons[2].product} link="saltnic" />
         <ListProduct
           heading="disposable pod"
-          data={disposablepod.product}
+          data={jsons[3].product}
           link="disposable-pod"
         />
-        <ListProduct heading="pod mod" data={podmod.product} link="pod-mod" />
+        <ListProduct heading="pod mod" data={jsons[4].product} link="pod-mod" />
 
         <ListProduct
           heading="box tank"
-          data={boxtank.product}
+          data={jsons[5].product}
           link="box-tank"
         />
 
         <ListProduct
           heading="phụ kiện"
-          data={phukien.product}
+          data={jsons[6].product}
           link="phu-kien"
         />
       </div>
@@ -64,50 +56,37 @@ const Home: NextPage = ({
 export default Home;
 
 export async function getServerSideProps(context: any) {
-  const resFreebase = await fetch(
-    "https://vape-store.herokuapp.com/api/product?page=1&&limit=4&&cat=freebase"
-  );
-  const freebase = await resFreebase.json();
+  const responses = await Promise.all([
+    fetch("https://vape-store.herokuapp.com/api/banner", {
+      method: "GET",
+    }),
+    fetch(
+      "https://vape-store.herokuapp.com/api/product?page=1&&limit=4&&cat=freebase"
+    ),
+    fetch(
+      "https://vape-store.herokuapp.com/api/product?page=1&&limit=4&&cat=saltnic"
+    ),
+    fetch(
+      "https://vape-store.herokuapp.com/api/product?page=1&&limit=4&&cat=disposable-pod"
+    ),
+    fetch(
+      "https://vape-store.herokuapp.com/api/product?page=1&&limit=4&&cat=pod-mod"
+    ),
+    fetch(
+      "https://vape-store.herokuapp.com/api/product?page=1&&limit=4&&cat=box-tank"
+    ),
+    fetch(
+      "https://vape-store.herokuapp.com/api/product?page=1&&limit=4&&cat=phu-kien"
+    ),
+  ]);
 
-  const resSaltnic = await fetch(
-    "https://vape-store.herokuapp.com/api/product?page=1&&limit=4&&cat=saltnic"
+  const jsons = await Promise.all(
+    await Promise.all(responses.map((res) => res.json()))
   );
-  const saltnic = await resSaltnic.json();
-
-  const resPodmod = await fetch(
-    "https://vape-store.herokuapp.com/api/product?page=1&&limit=4&&cat=pod-mod"
-  );
-  const podmod = await resPodmod.json();
-
-  const resBoxTank = await fetch(
-    "https://vape-store.herokuapp.com/api/product?page=1&&limit=4&&cat=box-tank"
-  );
-  const boxtank = await resBoxTank.json();
-
-  const resPhuKien = await fetch(
-    "https://vape-store.herokuapp.com/api/product?page=1&&limit=4&&cat=phu-kien"
-  );
-  const phukien = await resPhuKien.json();
-
-  const resDisposablePod = await fetch(
-    "https://vape-store.herokuapp.com/api/product?page=1&&limit=4&&cat=disposable-pod"
-  );
-  const disposablepod = await resDisposablePod.json();
-
-  const resBanner = await fetch("https://vape-store.herokuapp.com/api/banner", {
-    method: "GET",
-  });
-  const banner = await resBanner.json();
 
   return {
     props: {
-      freebase,
-      saltnic,
-      podmod,
-      boxtank,
-      phukien,
-      disposablepod,
-      banner,
+      jsons,
     },
   };
 }
